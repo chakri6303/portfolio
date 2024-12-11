009ce20adb5df64dd7adb8ea4c751a561097ebd3
chakri <ramakrishnachakri68@gmail.com>
2024-12-12 01:18:49 +0530

    Updates

---
+++ server/pages/_app.js
@@ -0,0 +1,74 @@
+(() => {
+var exports = {};
+exports.id = 888;
+exports.ids = [888];
+exports.modules = {
+
+/***/ 5892:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"style": {"fontFamily":"'__Poppins_ef1f1a', '__Poppins_Fallback_ef1f1a'","fontStyle":"normal"},
+	"className": "__className_ef1f1a"
+};
+
+
+/***/ }),
+
+/***/ 3847:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ App)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_font_google_target_css_path_pages_app_tsx_import_Poppins_arguments_weight_100_200_400_700_900_variableName_poppins___WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5892);
+/* harmony import */ var next_font_google_target_css_path_pages_app_tsx_import_Poppins_arguments_weight_100_200_400_700_900_variableName_poppins___WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_font_google_target_css_path_pages_app_tsx_import_Poppins_arguments_weight_100_200_400_700_900_variableName_poppins___WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6764);
+/* harmony import */ var _styles_globals_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_globals_css__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+function App({ Component , pageProps  }) {
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+        id: "root",
+        className: (next_font_google_target_css_path_pages_app_tsx_import_Poppins_arguments_weight_100_200_400_700_900_variableName_poppins___WEBPACK_IMPORTED_MODULE_2___default().className),
+        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(Component, {
+            ...pageProps
+        })
+    });
+}
+
+
+/***/ }),
+
+/***/ 6764:
+/***/ (() => {
+
+
+
+/***/ }),
+
+/***/ 997:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("react/jsx-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__(3847));
+module.exports = __webpack_exports__;
+
+})();
