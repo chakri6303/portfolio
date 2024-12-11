fabae385e48bb26471f25b8415572f182492cfec
chakri6303 <138191999+chakri6303@users.noreply.github.com>
2024-12-12 02:10:50 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,21 +7,21 @@ export default function home() {
       <Head>
         <title>Rama Krishna| Android Developer</title>
         <link rel="shortcut icon" href="/favicon.png" />
-        <meta name="description" content="John Carlo Devera | Frontend Developer" />
+        <meta name="description" content="Rama Krishna| Android Developer" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content="John Carlo Devera | Frontend Developer" />
-        <meta name="description" content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate." />
+        <meta property="og:title" content="Rama Krishna| Android Developer" />
+        <meta name="description" content="Hey! I'm Rama Krishna, and I'm a Bachelor of Technology graduate." />
 
-        <meta property="og:title" content="John Carlo Devera | Frontend Developer" />
+        <meta property="og:title" content="Rama Krishna| Android Developer" />
         <meta property="og:type" content="article" />
         <meta property="og:url" content="https://deverajc.com/" />
         <meta property="og:image" content="https://deverajc.com/screenshot.png" />
-        <meta property="og:description" content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate." />
+        <meta property="og:description" content="Hey! I'm Rama Krishna, and I'm a Bachelor of Technology graduate." />
 
         <meta name="twitter:card" content="summary"/>
         <meta name="twitter:site" content="@jc_devz"/>
-        <meta name="twitter:title" content="John Carlo Devera | Frontend Developer"/>
-        <meta name="twitter:description" content="Hey! I'm John Carlo Devera, and I'm a Bachelor of Science in Information Technology graduate."/>
+        <meta name="twitter:title" content="Rama Krishna| Android Developerr"/>
+        <meta name="twitter:description" content="Hey! I'm Rama Krishna, and I'm a Bachelor of Technology graduate."/>
         <meta name="twitter:creator" content="@jc_devz"/>
         <meta name="twitter:image" content="https://deverajc.com/screenshot.png"/>
 
